66a1e81fd80d9444643906eff423886521309ea6
Captain Joni <captain.joni@thepic.de>
2026-03-19 18:11:53 +0100

    idkman

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ app.use('/api/events', eventRoutes);
 app.use('/api', toppingRoutes);
 
 const PORT = process.env.PORT || 3000;
+//app.listen(PORT, () => console.log(`Server läuft auf ${PORT}`));
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server läuft auf Port ${PORT}`);
 });
